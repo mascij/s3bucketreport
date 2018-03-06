95f0221a4ff7aa810bdf342242fc77b490af1540
john masci <john.masci@gmail.com>
2018-03-06 10:34:20 -0700

    second commit

--- s3tklog.sh
+++ s3tklog.sh
@@ -4,15 +4,15 @@ date=$(date +%Y-%m-%d_%H:%M)
 
 echo "Running S3 Bucket Check for " $date
 
-/usr/local/bin/s3tk scan > /home/hf/s3log.$date.log
+/usr/local/bin/s3tk scan > /home/hf/s3bucketreport/log.$date.log
 
 echo "Waiting for Report to Generate"
 
-while [ ! -f /home/hf/s3log.$date.log ]
+while [ ! -f /home/hf/s3bucketreport/log.$date.log ]
 do
   sleep 2
 done
 
 echo "Sending Email"
 
-cat /home/hf/s3log.$date.log | mail -s "Daily S3 Bucket Security Report" -a "From: Admin Name <noreply@example.org>" jmasci@hypergrid.com
+cat /home/hf/s3bucketreport/log.$date.log | mail -s "Daily S3 Bucket Security Report" -a "From: Admin Name <noreply@example.org>" jmasci@hypergrid.com
